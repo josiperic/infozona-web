5a45d5bf8f5c9bcc12092ab3130ebe4070a4a519
josiperic <peric.dizajn@gmail.com>
2016-04-17 19:15:31 +0200

    hamburgers

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,8 @@
 (function(){
     // code goes here
     console.log('loaded script');
+    $(".header-toggle").on("click", function() {
+        $(".header-toggle").parent().toggleClass('active');
+        console.log("toggling");
+    });
 })();
